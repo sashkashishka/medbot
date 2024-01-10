59bd02aefadbd18b484ba15280928e3e241fd8c7
sashkashishka <alexandr1696@hotmail.com>
2024-01-10 19:23:59 +0100

    fix: delete on start command only user related orders, appointemnts

--- server/src/plugins/medbot/middlewares/cleanupOnSecondStartCommand.ts
+++ server/src/plugins/medbot/middlewares/cleanupOnSecondStartCommand.ts
@@ -1,52 +1,55 @@
-import type { MiddlewareFn } from 'telegraf';
+import type { MiddlewareFn, NarrowedContext, Types } from 'telegraf';
 import type { iMedbotContext } from '../types.js';
 import { SCENES } from '../constants/scenes.js';
 import { medbotLogger } from '../../../logger.js';
 
-export const cleanupOnSecondStartCommand: MiddlewareFn<iMedbotContext> =
-  async function cleanupOnSecondStartCommand(ctx, next) {
-    if (ctx.session?.__scenes?.current !== SCENES.ORDER) {
-      ctx.session = {};
+export const cleanupOnSecondStartCommand: MiddlewareFn<
+  NarrowedContext<iMedbotContext, Types.MountMap['text']>
+> = async function cleanupOnSecondStartCommand(ctx, next) {
+  if (ctx.session?.__scenes?.current !== SCENES.ORDER) {
+    ctx.session = {};
 
-      try {
-        const orders = await ctx.prisma.order.findMany({
-          where: { status: 'ACTIVE' },
-        });
-        const appointments = await ctx.prisma.appointment.findMany({
-          where: { status: 'ACTIVE' },
-        });
+    const { update } = ctx;
 
-        const pendingArr = [];
+    try {
+      const orders = await ctx.prisma.order.findMany({
+        where: { userId: update.message.from.id, status: 'ACTIVE' },
+      });
+      const appointments = await ctx.prisma.appointment.findMany({
+        where: { userId: update.message.from.id, status: 'ACTIVE' },
+      });
 
-        orders.forEach((order) => {
-          pendingArr.push(
-            ctx.prisma.order.update({
-              where: { id: order?.id },
-              data: { status: 'DONE' },
-            }),
-          );
-        });
-        appointments.map((appointment) => {
-          pendingArr.push(
-            ctx.prisma.appointment.update({
-              where: { id: appointment?.id },
-              data: { status: 'DELETED' },
-            }),
-          );
+      const pendingArr = [];
 
-          pendingArr.push(
-            ctx.googleCalendar.events.delete({
-              calendarId: ctx.googleCalendarId,
-              eventId: appointment.calendarEventId,
-            }),
-          );
-        });
+      orders.forEach((order) => {
+        pendingArr.push(
+          ctx.prisma.order.update({
+            where: { id: order?.id },
+            data: { status: 'DONE' },
+          }),
+        );
+      });
+      appointments.map((appointment) => {
+        pendingArr.push(
+          ctx.prisma.appointment.update({
+            where: { id: appointment?.id },
+            data: { status: 'DELETED' },
+          }),
+        );
 
-        await Promise.all(pendingArr);
-      } catch (e) {
-        medbotLogger.error(e, 'cleanupOnSecondStartCommand');
-      }
+        pendingArr.push(
+          ctx.googleCalendar.events.delete({
+            calendarId: ctx.googleCalendarId,
+            eventId: appointment.calendarEventId,
+          }),
+        );
+      });
+
+      await Promise.all(pendingArr);
+    } catch (e) {
+      medbotLogger.error(e, 'cleanupOnSecondStartCommand');
     }
+  }
 
-    return next();
-  };
+  return next();
+};
