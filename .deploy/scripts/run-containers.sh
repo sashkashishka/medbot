3a1ecb0aaa1c8192372b22d787abd8f35b10dfd6
sashkashishka <alexandr1696@hotmail.com>
2024-04-07 00:26:19 +0200

    ci: deploy 5

--- .deploy/scripts/run-containers.sh
+++ .deploy/scripts/run-containers.sh
@@ -1,21 +1,26 @@
 #! /bin/bash
 
 tag=$TAG
+imagesDir=$IMAGES_DIR
 dir=$DIR
 env=$ENV
 
+echo 'Load images';
+docker load -i ${imagesDir}/medbot/fastify
+docker load -i ${imagesDir}/medbot/nginx
+
 echo 'Stop previous';
-docker compose -f ~/$dir/.deploy/docker-compose.yaml stop db_$env adminer_$env fastify_$env nginx_medbot_$env
+docker compose -f $dir/.deploy/docker-compose.yaml stop db_$env adminer_$env fastify_$env nginx_medbot_$env
 
 echo 'Start db';
-docker compose -f ~/$dir/.deploy/docker-compose.yaml up db_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.yaml up db_$env -d --wait
 
 echo 'Start adminer';
-docker compose -f ~/$dir/.deploy/docker-compose.yaml up adminer_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.yaml up adminer_$env -d --wait
 
 echo 'Start fastify';
-docker compose -f ~/$dir/.deploy/docker-compose.yaml up fastify_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.yaml up fastify_$env -d --wait
 
 echo 'Start nginx';
-docker compose -f ~/$dir/.deploy/docker-compose.yaml up nginx_medbot_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.yaml up nginx_medbot_$env -d --wait
 
