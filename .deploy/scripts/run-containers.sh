fdc5c1e6ed165f6d9b893d21c309e6d599db0a57
sashkashishka <alexandr1696@hotmail.com>
2024-04-07 00:45:09 +0200

    ci: deploy 7

--- .deploy/scripts/run-containers.sh
+++ .deploy/scripts/run-containers.sh
@@ -6,8 +6,8 @@ dir=$DIR
 env=$ENV
 
 echo 'Load images';
-docker load -i ${imagesDir}/medbot/fastify
-docker load -i ${imagesDir}/medbot/nginx
+docker load -i $imagesDir/fastify
+docker load -i $imagesDir/nginx
 
 echo 'Stop previous';
 docker compose -f $dir/.deploy/docker-compose.yaml stop db_$env adminer_$env fastify_$env nginx_medbot_$env
