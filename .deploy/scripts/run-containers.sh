de4d84c4184e1e929fa14c909e1e4fa6c3740480
sashkashishka <alexandr1696@hotmail.com>
2024-04-07 02:01:42 +0200

    ci: 19

--- .deploy/scripts/run-containers.sh
+++ .deploy/scripts/run-containers.sh
@@ -10,17 +10,17 @@ docker load -i $imagesDir/fastify.tar
 docker load -i $imagesDir/nginx.tar
 
 echo 'Stop previous';
-docker compose -f $dir/.deploy/docker-compose.yaml stop db_$env adminer_$env fastify_$env nginx_medbot_$env
+docker compose -f $dir/.deploy/docker-compose.$env.yaml stop 
 
 echo 'Start db';
-docker compose -f $dir/.deploy/docker-compose.yaml up db_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.$env.yaml up db -d --wait
 
 echo 'Start adminer';
-docker compose -f $dir/.deploy/docker-compose.yaml up adminer_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.$env.yaml up adminer -d --wait
 
 echo 'Start fastify';
-docker compose -f $dir/.deploy/docker-compose.yaml up fastify_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.$env.yaml up fastify -d --wait
 
 echo 'Start nginx';
-docker compose -f $dir/.deploy/docker-compose.yaml up nginx_medbot_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.$env.yaml up nginx -d --wait
 
