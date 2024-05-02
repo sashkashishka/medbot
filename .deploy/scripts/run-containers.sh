49de84540006184e2a6bd5bfee0ed4b76cb9625d
sashkashishka <alexandr1696@hotmail.com>
2024-05-02 11:05:51 +0200

    test: check 2

--- .deploy/scripts/run-containers.sh
+++ .deploy/scripts/run-containers.sh
@@ -18,4 +18,4 @@ echo 'Start adminer';
 docker compose -f $dir/.deploy/docker-compose.$env.yaml up adminer_$env -d --wait
 
 echo 'Start fastify';
-docker compose -f $dir/.deploy/docker-compose.$env.yaml up fastify_$env -d --wait
+docker compose -f $dir/.deploy/docker-compose.$env.yaml up fastify_$env -d --wait || exit 1;
